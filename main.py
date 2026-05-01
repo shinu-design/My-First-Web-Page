9174cbb54a37fb1362c756619a90af2910db6e9c
shinu-design <shinu@miis.ae>
2026-05-02 00:15:19 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import streamlit as st
 st.title("My First Streamlit App")
-st.write("Hello!Creating a simple web application using Streamlit.")
+name=st.text_input("Enter your name:")
+if st.button("submit"):
+    st.write("Hello!{name}Welcome to  Streamlit.")
